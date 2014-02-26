264ecfe9bbd8a8022c86d7e370ff400b640b636b
Sebastian Raschka <se.raschka@me.com>
2014-02-26 16:30:40 -0500

    Update auto_unfollow.py

--- auto_unfollow.py
+++ auto_unfollow.py
@@ -61,7 +61,6 @@ def auto_unfollow(db_file):
                 if not check:              
                     c.execute('INSERT INTO twitter_db (user_id) VALUES ("%s")' %userid)              
                 print('unfollowed: %s' % t.users.lookup(user_id=userid)[0]['screen_name'])
-                print('Unfollowed %s users' %cnt)
         except Exception as e:
             print(e)
             conn.commit()
